13ae04b71374f09fa5e81b80fe122ef28df8b728
Yusuf Kenan <q@q.com>
2019-07-11 14:53:38 +0300

    .

--- base56Repetition/Program.cs
+++ base56Repetition/Program.cs
@@ -6,7 +6,7 @@ namespace base56Repetition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Repetition");
         }
     }
 }
